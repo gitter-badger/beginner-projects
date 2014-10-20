33b2446d7fe86eb121e746d325365f80fa1cb809
Austin Witherow <au.witherow@gmail.com>
2014-10-20 13:10:22 +0200

    really small progress on the css
    to unbuttify it.

--- 01-quote-switcher/quote_switch.js
+++ 01-quote-switcher/quote_switch.js
@@ -9,22 +9,11 @@ quotes.push({"content": "Two things are infinite: the universe and human stupidi
 quotes.push({"content": "If you tell the truth, you don't have to remember anything.", "cite": "Mark Twain"});
 
 // finally begin jQuery
+// var count = 0;
 $('button').click(function () {
-    var random = Math.floor(Math.random() * quotes.length); // picks a random number based on the length of the quote array.
+    var random = Math.floor(Math.random() * quotes.length);
+    // picks a random number based on the length of the quote array.
+    // var random MUST be local, otherwise random value will be static and not dynamic to the click.
     $('blockquote').text(quotes[random]['content']); // sticks the content into the blockquote.
     $('cite').text(quotes[random]['cite']); // sticks the citation into the cite.
 });
-
-// version one, no javascript, didn't have the functionality I wanted.
-//
-// function load_quote(){
-//     var quotes, random; // define variables
-//     // quotes array,
-//     quotes = [
-//         "Luck is what happens when preparation meets opportunity. - Seneca",
-//         "Begin at once to live, and count each separate day as a separate life. - Seneca",
-//         "It is a rough road that leads to the heights of greatness. - Seneca"
-//     ];
-//     random = quotes[Math.floor(Math.random() * quotes.length)];
-//     document.getElementById('quote').innerHTML = random;
-// };
