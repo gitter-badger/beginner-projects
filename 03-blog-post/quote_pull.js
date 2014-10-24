a3eeff5c3de7bcfb7c95c202ae8babcb69566ced
Austin Witherow <au.witherow@gmail.com>
2014-10-24 12:59:22 +0200

    simplified, iteration
    i sought some help on Stack Overflow on how to iterate through, instead
    of having two identical blocks of code (identical except for the pull
    left or pull right). Also simplified variables, classes, calls, etc.

---
+++ 03-blog-post/quote_pull.js
@@ -0,0 +1,14 @@
+// version 0.2, with help from StackExchange
+var right = pullQuote("right");
+var left = pullQuote("left");
+
+function pullQuote(direction) {
+  var qo = $('article').find('span.' + direction).each(function() {
+    var $this = $(this);
+
+    $('<blockquote></blockquote>', {
+      class: direction,
+      text: $this.text()
+    }).prependTo( $this.closest('p') );
+  });
+}
