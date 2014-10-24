f1325bf37b0015528d9ebc91eec28f60def251a3
Austin Witherow <au.witherow@gmail.com>
2014-10-24 13:11:22 +0200

    best practices fixes
    just made some fixes for best practices.

--- 03-blog-post/quote_pull.js
+++ 03-blog-post/quote_pull.js
@@ -1,14 +1,14 @@
 // version 0.2, with help from StackExchange
-var right = pullQuote("right");
-var left = pullQuote("left");
+var right = pullQuote("right"),
+    left = pullQuote("left");
 
 function pullQuote(direction) {
-  var qo = $('article').find('span.' + direction).each(function() {
+  var qo = $('article').find("span." + direction).each(function() {
     var $this = $(this);
 
-    $('<blockquote></blockquote>', {
+    $("<blockquote></blockquote>", {
       class: direction,
       text: $this.text()
-    }).prependTo( $this.closest('p') );
+    }).prependTo( $this.closest("p") );
   });
 }
